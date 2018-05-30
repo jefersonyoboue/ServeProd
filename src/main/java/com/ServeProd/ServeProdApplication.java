698b6ead20de0497b79c2c4d468f0cec3452cd9e
jefersonyoboue <jefersonyoboue@gmail.com>
2018-05-30 15:05:23 +0000

    first application

---
+++ src/main/java/com/ServeProd/ServeProdApplication.java
@@ -0,0 +1,12 @@
+package com.ServeProd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeProdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServeProdApplication.class, args);
+	}
+}
